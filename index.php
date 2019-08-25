0422aab2c0d18840569996f9e2caa259b5ade7e3
PonomareVlad <Git@PonomareVlad.ru>
2019-08-25 16:15:06 +0500

    Added Name field in Prototype

--- index.php
+++ index.php
@@ -2,19 +2,23 @@
 
 $phone = false;
 $count = false;
+$name = false;
 $additions = false;
 
 if (isset($_REQUEST['phone-3'])) $phone = $_REQUEST['phone-3'];
 if (isset($_REQUEST['Phone'])) $phone = $_REQUEST['Phone'];
 if (isset($_REQUEST['Koli4estvo'])) $count = $_REQUEST['Koli4estvo'];
+if (isset($_REQUEST['Name'])) $name = $_REQUEST['Name'];
 if (isset($_REQUEST['Dop-podarok']) && $_REQUEST['Dop-podarok'] == 'on') $additions = true;
 
 $request = 'https://maker.ifttt.com/trigger/FooksiaOrder/with/key/e90iC92t-8snoggxBuwYnhL3KqPGnfaNUtJDlE8nuA?';
 
-if (!$phone || !$count) exit(json_encode(['status' => false]));
+if (!$phone) exit(json_encode(['status' => false]));
 
-$request .= 'value1=' . $phone . '&value2=' . $count;
+$request .= 'value1=' . $phone . '&value2=' . ($count || 'Не указано');
 
-if ($additions) $request .= 'value3=Дополнительный подарок: Да';
+if ($name || $additions) $request .= 'value3=';
+if ($name) $request .= 'Имя: ' . $name . '<br>';
+if ($additions) $request .= 'Дополнительный подарок: Да';
 
 file_get_contents($request);
