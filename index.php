7e7c74597c71da50e4ba1f67a1d000219f592149
PonomareVlad <Git@PonomareVlad.ru>
2019-08-25 15:48:15 +0500

    Testing requests 2

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 var_dump($_REQUEST);
 
-file_get_contents('https://maker.ifttt.com/trigger/FooksiaOrder/with/key/e90iC92t-8snoggxBuwYnhL3KqPGnfaNUtJDlE8nuA?value1=' . time());
+file_get_contents('https://maker.ifttt.com/trigger/FooksiaOrder/with/key/e90iC92t-8snoggxBuwYnhL3KqPGnfaNUtJDlE8nuA?value1=' . json_encode($_REQUEST));
