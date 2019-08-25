ffaa180e1b768559590659ea3192798cacbe36fe
PonomareVlad <Git@PonomareVlad.ru>
2019-08-25 16:04:14 +0500

    Prototype

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
 
-var_dump($_REQUEST);
+$phone = false;
+$count = false;
+$additions = false;
 
-file_get_contents('https://maker.ifttt.com/trigger/FooksiaOrder/with/key/e90iC92t-8snoggxBuwYnhL3KqPGnfaNUtJDlE8nuA?value1=' . json_encode($_REQUEST));
+if (isset($_REQUEST['phone-3'])) $phone = $_REQUEST['phone-3'];
+if (isset($_REQUEST['Phone'])) $phone = $_REQUEST['Phone'];
+if (isset($_REQUEST['Koli4estvo'])) $count = $_REQUEST['Koli4estvo'];
+if (isset($_REQUEST['Dop-podarok']) && $_REQUEST['Dop-podarok'] == 'on') $additions = true;
+
+$request = 'https://maker.ifttt.com/trigger/FooksiaOrder/with/key/e90iC92t-8snoggxBuwYnhL3KqPGnfaNUtJDlE8nuA?';
+
+if (!$phone || !$count) exit(json_encode(['status' => false]));
+
+$request .= 'value1=' . $phone . '&value2=' . $count;
+
+if ($additions) $request .= 'value3=Дополнительный подарок: Да';
+
+file_get_contents($request);
