b80fda26794e84d0d99939c15f4cc8e6eeb28f34
PonomareVlad <Git@PonomareVlad.ru>
2019-10-08 17:03:57 +0500

    Debug

--- endpoints/ne-bolno.ru.php
+++ endpoints/ne-bolno.ru.php
@@ -5,14 +5,15 @@ $data = getRequestVars([
     'name',
     'date'
 ]);
-
+echo response($data);
 $data['name'] = empty($data['name']) ? null : 'Имя: ' . $data['name'];
 $data['date'] = empty($data['date']) ? null : 'Дата: ' . $data['date'];
 $data['phone'] = empty($data['phone']) ? null : 'Телефон: <a href="tel:' . $data['phone'] . '">' . $data['phone'] . '</a>';
-
-echo response(['status' => sendMail('sys.system@mail.ru', 'Новая заявка на сайте ne-bolno.ru',
-    join('<br>', array_filter([
-        $data['name'],
-        $data['phone'],
-        $data['date']
-    ])))]);
+echo response($data);
+$message = join('<br>', array_filter([
+    $data['name'],
+    $data['phone'],
+    $data['date']
+]));
+echo response($message);
+echo response(['status' => sendMail('sys.system@mail.ru', 'Новая заявка на сайте ne-bolno.ru', $message)]);
