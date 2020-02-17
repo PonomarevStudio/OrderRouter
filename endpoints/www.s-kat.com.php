437d60e524cb117eb3badd3a5472acb642a23e53
PonomareVlad <Git@PonomareVlad.ru>
2020-02-17 12:52:08 +0500

    New prototype for S-kat.com

--- endpoints/www.s-kat.com.php
+++ endpoints/www.s-kat.com.php
@@ -1,11 +1,11 @@
 <?php
 
 $dataItems = ['',
-    'Выберите гранулометрический состав материала подвергаемого флотационному обогащению',
-    'Кислотность пульпы, рН',
-    'Соотношение Т:Ж в пульпе',
-    'Объем пульпы, м3/час',
-    'Химический, минералогический и фазовый состав руды'];
+    '1.1. Гранулометрический состав материала подвергаемого флотационному обогащению',
+    '1.2. Кислотность пульпы, рН',
+    '1.3. Соотношение Т:Ж в пульпе',
+    '1.4. Объем пульпы, м3/час',
+    '1.5. Химический, минералогический и фазовый состав руды'];
 
 $data = getRequestVars([
     'raw',
@@ -37,10 +37,10 @@ $data['name'] = empty($data['name']) ? null : 'Имя: ' . $data['name'];
 $data['email'] = empty($data['email']) ? null : 'Email: ' . $data['email'];
 $data['phone'] = empty($data['phone']) ? null : 'Телефон: <a href="tel:' . intval($data['phone']) . '">' . $data['phone'] . '</a>';
 $data['message'] = empty($data['message']) ? null : 'Сообещение: ' . $data['message'];
-$data['2'] = empty($data['2']) ? null : 'Выберите предполагаемое место установки в схеме флотационного обогащения: ' . $data['2'];
-$data['otherPlace'] = empty($data['otherPlace']) ? null : 'Другое место установки: ' . $data['otherPlace'];
-$data['3'] = empty($data['3']) ? null : 'Требуется ли вам насос для перекачивания пульпы с производительностью, превышающей перерабатываемый объем пульпы в 3 раза и обеспечивающий давление не ниже 0,2 МПа: ' . $data['3'];
-$data['4'] = empty($data['4']) ? null : 'Требуется ли вам сжатый воздух с давлением не менее 0,05 МПа в пятикратном объеме от объема перерабатываемой пульпы: ' . $data['4'];
+$data['otherPlace'] = empty($data['otherPlace']) ? null : $data['otherPlace'];
+$data['2'] = empty($data['2']) ? null : 'Предполагаемое место установки в схеме флотационного обогащения: ' . is_null($data['otherPlace']) ? $data['2'] : $data['otherPlace'];
+$data['3'] = empty($data['3']) ? null : 'Требуется ли насос для перекачивания пульпы с производительностью, превышающей перерабатываемый объем пульпы в 3 раза и обеспечивающий давление не ниже 0,2 МПа: ' . $data['3'];
+$data['4'] = empty($data['4']) ? null : 'Требуется ли сжатый воздух с давлением не менее 0,05 МПа в пятикратном объеме от объема перерабатываемой пульпы: ' . $data['4'];
 $data['otherRawTitle'] = empty($data['otherRawTitle']) ? null : 'Другое сырье: ' . $data['otherRawTitle'];
 $data['raw'] = empty($data['raw']) ? null : 'Выберите минеральное сырье для флотационного обогащения: ' . join(', ', $data['raw']);
 
@@ -49,12 +49,16 @@ $message = join('<br>', array_filter([
     $data['email'],
     $data['phone'],
     $data['message'],
-    $data['raw'],
-    $data['otherRawTitle'],
+    '<br>',
     $data['2'],
-    $data['otherPlace'],
+    '<br>',
     $data['3'],
+    '<br>',
     $data['4'],
+    '<br>',
+//    $data['raw'],
+//    $data['otherRawTitle'],
+//    $data['otherPlace'],
     $data['rawReadableData']]));
 
-echo response(['status' => sendMail('antonkrypton@gmail.com', 'Новая заявка на сайте S-kat', $message)]);
+echo response(['status' => sendMail('sys.system@mail.ru', 'Новая заявка на сайте S-kat', $message)]);
