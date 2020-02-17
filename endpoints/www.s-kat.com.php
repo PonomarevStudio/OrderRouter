d2f85af365b17031d2a6c6795e7caff41a960db7
PonomareVlad <Git@PonomareVlad.ru>
2020-02-17 12:59:15 +0500

    Updated prototype for S-kat.com

--- endpoints/www.s-kat.com.php
+++ endpoints/www.s-kat.com.php
@@ -28,19 +28,19 @@ if (isset($data['raw'])) foreach ($data['raw'] as $raw) {
 }
 
 foreach ($data['rawData'] as $raw => &$rawData) {
-    foreach ($rawData as $item => $value) if ($value == '') $rawData[$item] = null; else $rawData[$item] = $dataItems[intval($item)] . ': ' . $value;
+    foreach ($rawData as $item => $value) if ($value == '') $rawData[$item] = null; else $rawData[$item] = $dataItems[intval($item)] . ': <b>' . $value . '</b>';
     $rawData = join('<br>', array_filter([$rawData['1'], $rawData['2'], $rawData['3'], $rawData['4'], $rawData['5']]));
-    $data['rawReadableData'] .= '<br>' . $raw . ':<br>' . $rawData;
+    $data['rawReadableData'] .= '<br><b>' . $raw . '</b>:<br>' . $rawData;
 }
 
-$data['name'] = empty($data['name']) ? null : 'Имя: ' . $data['name'];
-$data['email'] = empty($data['email']) ? null : 'Email: ' . $data['email'];
-$data['phone'] = empty($data['phone']) ? null : 'Телефон: <a href="tel:' . intval($data['phone']) . '">' . $data['phone'] . '</a>';
-$data['message'] = empty($data['message']) ? null : 'Сообещение: ' . $data['message'];
+$data['name'] = empty($data['name']) ? null : 'Имя: <b>' . $data['name'] . '</b>';
+$data['email'] = empty($data['email']) ? null : 'Email: <b>' . $data['email'] . '</b>';
+$data['phone'] = empty($data['phone']) ? null : 'Телефон: <b><a href="tel:' . intval($data['phone']) . '">' . $data['phone'] . '</a></b>';
+$data['message'] = empty($data['message']) ? null : 'Сообещение: <b>' . $data['message'] . '</b>';
 $data['otherPlace'] = empty($data['otherPlace']) ? null : $data['otherPlace'];
-$data['2'] = empty($data['2']) ? null : 'Предполагаемое место установки в схеме флотационного обогащения: ' . is_null($data['otherPlace']) ? $data['2'] : $data['otherPlace'];
-$data['3'] = empty($data['3']) ? null : 'Требуется ли насос для перекачивания пульпы с производительностью, превышающей перерабатываемый объем пульпы в 3 раза и обеспечивающий давление не ниже 0,2 МПа: ' . $data['3'];
-$data['4'] = empty($data['4']) ? null : 'Требуется ли сжатый воздух с давлением не менее 0,05 МПа в пятикратном объеме от объема перерабатываемой пульпы: ' . $data['4'];
+$data['2'] = empty($data['2']) ? null : 'Предполагаемое место установки в схеме флотационного обогащения: <b>' . (is_null($data['otherPlace']) ? $data['2'] : $data['otherPlace']) . '</b>';
+$data['3'] = empty($data['3']) ? null : 'Требуется ли насос для перекачивания пульпы с производительностью, превышающей перерабатываемый объем пульпы в 3 раза и обеспечивающий давление не ниже 0,2 МПа: <b>' . $data['3'] . '</b>';
+$data['4'] = empty($data['4']) ? null : 'Требуется ли сжатый воздух с давлением не менее 0,05 МПа в пятикратном объеме от объема перерабатываемой пульпы: <b>' . $data['4'] . '</b>';
 $data['otherRawTitle'] = empty($data['otherRawTitle']) ? null : 'Другое сырье: ' . $data['otherRawTitle'];
 $data['raw'] = empty($data['raw']) ? null : 'Выберите минеральное сырье для флотационного обогащения: ' . join(', ', $data['raw']);
 
@@ -55,7 +55,7 @@ $message = join('<br>', array_filter([
     $data['3'],
     '<br>',
     $data['4'],
-    '<br>',
+//    '<br>',
 //    $data['raw'],
 //    $data['otherRawTitle'],
 //    $data['otherPlace'],
