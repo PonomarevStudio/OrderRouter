d1aa2e889f60001814f1997cf2c7abca3b72f81e
PonomareVlad <Git@PonomareVlad.ru>
2020-07-10 22:30:36 +0500

    Updated test endpoint

--- endpoints/test.php
+++ endpoints/test.php
@@ -2,11 +2,21 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
+$data = getRequestVars(['phone', 'name', 'date']);
+
+$data['name'] = empty($data['name']) ? null : 'Имя: ' . $data['name'];
+$data['date'] = empty($data['date']) ? null : 'Дата: ' . $data['date'];
+$data['phone'] = empty($data['phone']) ? null : 'Телефон: <a href="tel:' . intval($data['phone']) . '">' . $data['phone'] . '</a>';
+
+$message = join('<br>', array_filter([$data['name'], $data['phone'], $data['date']]));
+
 $data = [];
 
 foreach ($_REQUEST as $key => $value) {
     if (isset($value)) $data[] = "$key: $value";
 }
+
+$data['message'] = $message;
 
 $message = join('<br>', $data);
 
