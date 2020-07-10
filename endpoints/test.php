db7ba8e9ca1405be69bdc2664fa5b62ff3a6ccb0
PonomareVlad <Git@PonomareVlad.ru>
2020-07-10 20:53:59 +0500

    Updated test endpoint

--- endpoints/test.php
+++ endpoints/test.php
@@ -1,11 +1,11 @@
 <?php
 
-$data = getRequestVars(['phone', 'name', 'date']);
+//$data = getRequestVars(['phone', 'name', 'date']);
 
-$data['name'] = empty($data['name']) ? null : 'Имя: ' . $data['name'];
-$data['date'] = empty($data['date']) ? null : 'Дата: ' . $data['date'];
-$data['phone'] = empty($data['phone']) ? null : 'Телефон: <a href="tel:' . intval($data['phone']) . '">' . $data['phone'] . '</a>';
+//$data['name'] = empty($data['name']) ? null : 'Имя: ' . $data['name'];
+//$data['date'] = empty($data['date']) ? null : 'Дата: ' . $data['date'];
+//$data['phone'] = empty($data['phone']) ? null : 'Телефон: <a href="tel:' . intval($data['phone']) . '">' . $data['phone'] . '</a>';
 
-$message = join('<br>', array_filter([$data['name'], $data['phone'], $data['date']]));
+//$message = join('<br>', array_filter([$data['name'], $data['phone'], $data['date']]));
 
-echo response(['status' => sendMail('sys.system@mail.ru', 'Test', $message, true)]);
+echo response(['status' => sendMail('sys.system@mail.ru', 'Test', 'Test', true)]);
