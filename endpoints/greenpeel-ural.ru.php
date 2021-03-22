903cfc738f8e7be846c1dcb790a86d05ffe9a12d
PonomareVlad <Git@PonomareVlad.ru>
2021-03-22 18:26:07 +0500

    Update

--- endpoints/greenpeel-ural.ru.php
+++ endpoints/greenpeel-ural.ru.php
@@ -4,6 +4,10 @@
 
 $data = getRequestVars(['Email']);
 
-$message = file_get_contents(__DIR__ . '../templates/greenpeel-ural.ru.html');
+error_log(print_r($data, true));
+
+$message = file_get_contents(__DIR__ . '/../templates/greenpeel-ural.ru.html');
+
+error_log('Message length: ' . strlen($message));
 
 echo response(['status' => sendMail($data['Email'], 'Спасибо за регистрацию!', $message)]);
